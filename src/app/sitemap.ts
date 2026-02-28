69cc95b9893154a5093fa1fdb8f746ab1f63bcd2
Sagar Chauhan <tecorbitron@gmail.com>
2026-02-28 23:45:46 +0530

    Create next.js project - set mulish font - set global.css file - create robots.ts file - create sitemap.ts - created empty Footer.tsx and Header.tsx layout components for future development. - Implemented Google Analytics component in Analytics.tsx for tracking with dynamic GA ID. - added SVG logo file's to the logo-kit inside assets <src.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,56 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = "https://www.tecorbitron.com";
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/services`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.9,
+        },
+        {
+            url: `${baseUrl}/portfolio`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.7,
+        },
+        {
+            url: `${baseUrl}/blogs`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.6,
+        },
+        {
+            url: `${baseUrl}/privacy-policy`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.3,
+        },
+        {
+            url: `${baseUrl}/terms-and-conditions`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.3,
+        },
+    ];
+}
